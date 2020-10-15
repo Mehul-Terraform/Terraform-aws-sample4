ac95706a5d57bd06f3e7ca8ec9954f4695e3defe
MehulSolanki <er.mehulsolanki1887@gmail.com>
2020-10-15 15:11:38 +0530

    remove linear format

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "KCS-AWS-TF01" {
   ami           = "ami-0010d386b82bc06f0"
   key_name      = "KCS-AWS-TF-KeyPair"
   instance_type = "t2.nano"
-  vpc_security_group_ids = ["${aws_security_group.KCS-AWS-TF-SG.id}"]
+  vpc_security_group_ids = aws_security_group.KCS-AWS-TF-SG.id
   tags = {
     Name = "KCS-AWS-TF01"
     }
@@ -13,7 +13,7 @@ resource "aws_instance" "KCS-AWS-TF01" {
   ami           = "ami-0010d386b82bc06f0"
   key_name      = "KCS-AWS-TF-KeyPair"
   instance_type = "t2.nano"
-  vpc_security_group_ids = ["${aws_security_group.KCS-AWS-TF-SG.id}"]
+  vpc_security_group_ids = aws_security_group.KCS-AWS-TF-SG.id
   tags = {
     Name = "KCS-AWS-TF02"
     }
